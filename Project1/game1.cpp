b5459ff7b6374a2a577aa8e3e8071c48e90b5463
Bogdan <giboiq200@gmail.com>
2026-03-16 18:15:47 +0100

    added almost full game logic, missing score and textures and more enemies

--- Project1/game1.cpp
+++ Project1/game1.cpp
@@ -1,6 +1,36 @@
 ﻿#include <raylib.h>
 #include <iostream>
+#include <vector>
+//enemy
+struct Enemy {
+	float speed = 4;
+	float enemyX = 600;
+	float enemY = 0;
+	int enemyH = 40;
+	int enemyW = 20;
+};
+//player
+struct Player
+{
+	float playerx = 100;
+	float playery = 0;
+	bool isonGround = true;
+	int playerheight = 40;
+	int playerwidth = 20;
 
+	float jumpforce = 13;
+	float velocity = 0;
+	float gravity = 0.6;
+};
+//ground
+struct Ground
+{
+	int groundX = 0;
+	int groundY = 0;
+	int groundW = 0;
+	int groundH = 0;
+};
+//game state 
 enum GameState
 {
 	Menu,
@@ -15,6 +45,18 @@ int main() {
 	InitWindow(WindowWidth, WindowHeight, "Dinosaurus game!");
 	SetTargetFPS(60);
 	GameState currentState = Menu;
+	Ground ground1;
+	ground1.groundX = 0;
+	ground1.groundY = WindowHeight-30;
+	ground1.groundH = 30;
+	ground1.groundW = WindowWidth;
+	Player dinosaurus;
+	dinosaurus.playery = WindowHeight - 70;
+	Enemy enemy;
+	enemy.enemY = ground1.groundY - 40;
+	Rectangle playerRect;
+	Rectangle enemyRect;
+
 
 	while (!WindowShouldClose()) {
 		//Update logic
@@ -29,6 +71,38 @@ int main() {
 			if (IsKeyPressed(KEY_P)) {
 				currentState = Pause;
 			}
+
+			if (IsKeyPressed(KEY_SPACE) && dinosaurus.isonGround) {
+				dinosaurus.velocity = -dinosaurus.jumpforce;
+				dinosaurus.isonGround = false;
+			}
+			if (!dinosaurus.isonGround) {
+				dinosaurus.velocity += dinosaurus.gravity;
+				dinosaurus.playery += dinosaurus.velocity;
+			}
+			if (dinosaurus.playery + dinosaurus.playerheight >= ground1.groundY) {
+				dinosaurus.velocity = 0;
+				dinosaurus.isonGround = true;
+				dinosaurus.playery = ground1.groundY - dinosaurus.playerheight;
+			}
+			enemy.enemyX -= enemy.speed;
+			if (enemy.enemyX + enemy.enemyW < 0) {
+				enemy.enemyX = WindowWidth;
+			}
+
+			playerRect.x = dinosaurus.playerx;
+			playerRect.y = dinosaurus.playery;
+			playerRect.width = dinosaurus.playerwidth;
+			playerRect.height = dinosaurus.playerheight;
+
+			enemyRect.x = enemy.enemyX;
+			enemyRect.y = enemy.enemY;
+			enemyRect.width = enemy.enemyW;
+			enemyRect.height = enemy.enemyH;
+
+			if (CheckCollisionRecs(playerRect, enemyRect)) {
+				currentState = GameOver;
+			}
 			break;
 		case Pause:
 			if (IsKeyPressed(KEY_P)) {
@@ -36,12 +110,24 @@ int main() {
 			}
 			break;
 		case GameOver:
+			if (IsKeyPressed(KEY_SPACE)) {
+				dinosaurus.playery = ground1.groundY - dinosaurus.playerheight;
+				dinosaurus.velocity = 0;
+				dinosaurus.isonGround = true;
+
+				// Reset Enemy
+				enemy.enemyX = WindowWidth; // vraćamo neprijatelja sa desne strane
+				enemy.enemY = ground1.groundY - enemy.enemyH;
+
+				// Promeni stanje igre na PlayGame
+				currentState = PlayGame;
+			}
 			break;
 		default:
 			break;
 		}
 		BeginDrawing();
-		ClearBackground(RED);
+		ClearBackground(BLACK);
 		switch (currentState)
 		{
 		case Menu:
@@ -49,12 +135,17 @@ int main() {
 			DrawText("Press Escape to exit!", WindowWidth / 2-200, WindowHeight / 2 + 100, 30, YELLOW);
 			break;
 		case PlayGame:
-			DrawText("Game is playing", WindowWidth / 2-200, WindowHeight / 2, 40, GREEN);
+			DrawRectangle(ground1.groundX, ground1.groundY, ground1.groundW, ground1.groundH, WHITE);
+			DrawRectangle(dinosaurus.playerx, dinosaurus.playery, dinosaurus.playerwidth, dinosaurus.playerheight, GREEN);
+			DrawRectangle(enemy.enemyX, enemy.enemY, enemy.enemyW, enemy.enemyH, RED);
+
+
 			break;
 		case Pause:
 			DrawText("Game is paused", WindowWidth / 2-200, WindowHeight / 2, 40, BLUE);
 			break;
 		case GameOver:
+			DrawText("You lost, press space to restart!", WindowWidth / 2 - 200, WindowHeight / 2, 30, RED);
 			break;
 		default:
 			break;
