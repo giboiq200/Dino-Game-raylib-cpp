a6770a5426704ad79a76a2d14d97f4cb919646db
Bogdan <giboiq200@gmail.com>
2026-03-16 12:35:19 +0100

    Add project files.

---
+++ Project1/game1.cpp
@@ -0,0 +1,17 @@
+﻿#include <raylib.h>
+#include <iostream>
+
+int main() {
+	int WindowHeight = 600;
+	int WindowWidth = 800;
+	InitWindow(WindowWidth, WindowHeight, "Dinosaurus game!");
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose()) {
+		//Update logic
+		BeginDrawing();
+		ClearBackground(RED);
+		EndDrawing();
+	}
+	CloseWindow();
+}
